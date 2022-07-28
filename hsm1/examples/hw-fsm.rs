1eaadc4019f17999672d62cc918db4935576c394
Wink Saville <wink@saville.com>
2022-07-27 17:19:07 -0700

    feat: Add minimal examples

---
+++ hsm1/examples/hw-fsm.rs
@@ -0,0 +1,22 @@
+use hsm1::{handled, hsm1, hsm1_state, StateResult};
+
+struct NoMessages;
+
+hsm1!(
+    struct Fsm {
+        a_field: i32,
+    }
+
+    #[hsm1_state]
+    fn initial(&mut self, msg: &NoMessages) -> StateResult!() {
+        println!("Hello World, Fsm::initial: a_field={}", self.a_field);
+
+        handled!()
+    }
+);
+
+fn main() {
+    let mut fsm = Fsm::new();
+    let msg = NoMessages;
+    fsm.dispatch(&msg);
+}
