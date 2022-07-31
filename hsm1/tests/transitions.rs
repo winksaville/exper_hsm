df28cc6470c18ac2bd7ec9d59cb411fee5935308
Wink Saville <wink@saville.com>
2022-07-30 21:15:18 -0700

    test: transition tests
    Currently failing hsm1 tests
    transitions test_transition_to_between_leafs_of_trees
    
     wink@3900x 22-07-31T19:26:50.532Z:~/prgs/rust/myrepos/proc-macro-hsm1/hsm1 (transtion-tests-rebase|REBASE 2/3)
     $ cargo test --test transitions test_transition_to_between_leafs_of_trees
         Finished test [unoptimized + debuginfo] target(s) in 0.02s
          Running tests/transitions.rs (/home/wink/prgs/rust/myrepos/proc-macro-hsm1/target/debug/deps/transitions-c000e599b73f1a47)
    
     running 1 test
     test test_transition_to_between_leafs_of_trees ... FAILED
    
     failures:
    
     ---- test_transition_to_between_leafs_of_trees stdout ----
     thread 'test_transition_to_between_leafs_of_trees' panicked at 'assertion failed: `(left == right)`
       left: `1`,
      right: `2`', hsm1/tests/transitions.rs:266:5
     note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
    
    
     failures:
         test_transition_to_between_leafs_of_trees
    
     test result: FAILED. 0 passed; 1 failed; 0 ignored; 0 measured; 2 filtered out; finished in 0.00s
    
     error: test failed, to rerun pass '--test transitions'
    
    
    Currently failing in hsm0/main:
    
     wink@3900x 22-07-31T19:24:45.966Z:~/prgs/rust/myrepos/proc-macro-hsm1/hsm0 (transtion-tests-rebase|REBASE 2/3)
     $ cargo run
        Compiling hsm0 v0.1.0 (/home/wink/prgs/rust/myrepos/proc-macro-hsm1/hsm0)
         Finished dev [unoptimized + debuginfo] target(s) in 0.45s
          Running `/home/wink/prgs/rust/myrepos/proc-macro-hsm1/target/debug/hsm0`
     main
     thread 'main' panicked at 'assertion failed: `(left == right)`
       left: `1`,
      right: `2`', hsm0/src/main.rs:466:5
      note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
      failures:
    
      ---- test_transition_to_between_leafs_of_trees stdout ----
      thread 'test_transition_to_between_leafs_of_trees' panicked at 'assertion failed: `(left == right)`
        left: `1`,
       right: `2`', hsm1/tests/transitions.rs:266:5
      note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace

---
+++ hsm1/tests/transitions.rs
@@ -0,0 +1,306 @@
+//use hsm1::{handled, hsm1, hsm1_state, not_handled, transition_to, StateResult};
+use hsm1::{handled, hsm1, hsm1_state, transition_to, StateResult};
+
+struct NoMessages;
+
+#[test]
+fn test_transition_to_self_one_state() {
+    hsm1!(
+        struct Test {
+            initial_enter_cnt: usize,
+            initial_cnt: usize,
+            initial_exit_cnt: usize,
+        }
+
+        fn initial_enter(&mut self, _msg: &NoMessages) {
+            self.initial_enter_cnt += 1;
+        }
+
+        #[hsm1_state]
+        // This state has hdl 0
+        fn initial(&mut self, _msg: &NoMessages) -> StateResult!() {
+            self.initial_cnt += 1;
+            transition_to!(initial)
+        }
+
+        fn initial_exit(&mut self, _msg: &NoMessages) {
+            self.initial_exit_cnt += 1;
+        }
+    );
+
+    let mut fsm = Test::new();
+    assert_eq!(fsm.initial_enter_cnt, 0);
+    assert_eq!(fsm.initial_cnt, 0);
+    assert_eq!(fsm.initial_exit_cnt, 0);
+
+    fsm.dispatch(&NoMessages);
+    assert_eq!(fsm.initial_enter_cnt, 1);
+    assert_eq!(fsm.initial_cnt, 1);
+    assert_eq!(fsm.initial_exit_cnt, 1);
+
+    fsm.dispatch(&NoMessages);
+    assert_eq!(fsm.initial_enter_cnt, 2);
+    assert_eq!(fsm.initial_cnt, 2);
+    assert_eq!(fsm.initial_exit_cnt, 2);
+
+    fsm.dispatch(&NoMessages);
+    assert_eq!(fsm.initial_enter_cnt, 3);
+    assert_eq!(fsm.initial_cnt, 3);
+    assert_eq!(fsm.initial_exit_cnt, 3);
+}
+
+#[test]
+fn test_transition_to_between_two_unrelated_states() {
+    hsm1!(
+        struct Test {
+            initial_enter_cnt: usize,
+            initial_cnt: usize,
+            initial_exit_cnt: usize,
+            other_enter_cnt: usize,
+            other_cnt: usize,
+            other_exit_cnt: usize,
+        }
+
+        fn initial_enter(&mut self, _msg: &NoMessages) {
+            self.initial_enter_cnt += 1;
+        }
+
+        #[hsm1_state]
+        // This state has hdl 0
+        fn initial(&mut self, _msg: &NoMessages) -> StateResult!() {
+            self.initial_cnt += 1;
+            transition_to!(other)
+        }
+
+        fn initial_exit(&mut self, _msg: &NoMessages) {
+            self.initial_exit_cnt += 1;
+        }
+
+        fn other_enter(&mut self, _msg: &NoMessages) {
+            self.other_enter_cnt += 1;
+        }
+
+        #[hsm1_state]
+        // This state has hdl 0
+        fn other(&mut self, _msg: &NoMessages) -> StateResult!() {
+            self.other_cnt += 1;
+            transition_to!(initial)
+        }
+
+        fn other_exit(&mut self, _msg: &NoMessages) {
+            self.other_exit_cnt += 1;
+        }
+    );
+
+    let mut fsm = Test::new();
+    assert_eq!(fsm.initial_enter_cnt, 0);
+    assert_eq!(fsm.initial_cnt, 0);
+    assert_eq!(fsm.initial_exit_cnt, 0);
+    assert_eq!(fsm.other_enter_cnt, 0);
+    assert_eq!(fsm.other_cnt, 0);
+    assert_eq!(fsm.other_exit_cnt, 0);
+
+    fsm.dispatch(&NoMessages);
+    assert_eq!(fsm.initial_enter_cnt, 1);
+    assert_eq!(fsm.initial_cnt, 1);
+    assert_eq!(fsm.initial_exit_cnt, 1);
+    assert_eq!(fsm.other_enter_cnt, 0);
+    assert_eq!(fsm.other_cnt, 0);
+    assert_eq!(fsm.other_exit_cnt, 0);
+
+    fsm.dispatch(&NoMessages);
+    assert_eq!(fsm.initial_enter_cnt, 1);
+    assert_eq!(fsm.initial_cnt, 1);
+    assert_eq!(fsm.initial_exit_cnt, 1);
+    assert_eq!(fsm.other_enter_cnt, 1);
+    assert_eq!(fsm.other_cnt, 1);
+    assert_eq!(fsm.other_exit_cnt, 1);
+
+    fsm.dispatch(&NoMessages);
+    assert_eq!(fsm.initial_enter_cnt, 2);
+    assert_eq!(fsm.initial_cnt, 2);
+    assert_eq!(fsm.initial_exit_cnt, 2);
+    assert_eq!(fsm.other_enter_cnt, 1);
+    assert_eq!(fsm.other_cnt, 1);
+    assert_eq!(fsm.other_exit_cnt, 1);
+
+    fsm.dispatch(&NoMessages);
+    assert_eq!(fsm.initial_enter_cnt, 2);
+    assert_eq!(fsm.initial_cnt, 2);
+    assert_eq!(fsm.initial_exit_cnt, 2);
+    assert_eq!(fsm.other_enter_cnt, 2);
+    assert_eq!(fsm.other_cnt, 2);
+    assert_eq!(fsm.other_exit_cnt, 2);
+
+    fsm.dispatch(&NoMessages);
+    assert_eq!(fsm.initial_enter_cnt, 3);
+    assert_eq!(fsm.initial_cnt, 3);
+    assert_eq!(fsm.initial_exit_cnt, 3);
+    assert_eq!(fsm.other_enter_cnt, 2);
+    assert_eq!(fsm.other_cnt, 2);
+    assert_eq!(fsm.other_exit_cnt, 2);
+}
+
+#[test]
+fn test_transition_to_between_leafs_of_trees() {
+    hsm1!(
+        struct Test {
+            initial_base_enter_cnt: usize,
+            initial_base_cnt: usize,
+            initial_base_exit_cnt: usize,
+            initial_enter_cnt: usize,
+            initial_cnt: usize,
+            initial_exit_cnt: usize,
+            other_base_enter_cnt: usize,
+            other_base_cnt: usize,
+            other_base_exit_cnt: usize,
+            other_enter_cnt: usize,
+            other_cnt: usize,
+            other_exit_cnt: usize,
+        }
+
+        fn initial_base_enter(&mut self, _msg: &NoMessages) {
+            self.initial_base_enter_cnt += 1;
+        }
+
+        #[hsm1_state]
+        // This state has hdl 0
+        fn initial_base(&mut self, _msg: &NoMessages) -> StateResult!() {
+            self.initial_base_cnt += 1;
+            handled!()
+        }
+
+        fn initial_base_exit(&mut self, _msg: &NoMessages) {
+            self.initial_base_exit_cnt += 1;
+        }
+
+        fn initial_enter(&mut self, _msg: &NoMessages) {
+            self.initial_enter_cnt += 1;
+        }
+
+        #[hsm1_state(initial_base)]
+        // This state has hdl 0
+        fn initial(&mut self, _msg: &NoMessages) -> StateResult!() {
+            self.initial_cnt += 1;
+            transition_to!(other)
+        }
+
+        fn initial_exit(&mut self, _msg: &NoMessages) {
+            self.initial_exit_cnt += 1;
+        }
+
+        fn other_base_enter(&mut self, _msg: &NoMessages) {
+            self.other_base_enter_cnt += 1;
+        }
+
+        #[hsm1_state]
+        // This state has hdl 0
+        fn other_base(&mut self, _msg: &NoMessages) -> StateResult!() {
+            self.other_base_cnt += 1;
+            handled!()
+        }
+
+        fn other_base_exit(&mut self, _msg: &NoMessages) {
+            self.other_base_exit_cnt += 1;
+        }
+
+        fn other_enter(&mut self, _msg: &NoMessages) {
+            self.other_enter_cnt += 1;
+        }
+
+        #[hsm1_state(other_base)]
+        // This state has hdl 0
+        fn other(&mut self, _msg: &NoMessages) -> StateResult!() {
+            self.other_cnt += 1;
+            transition_to!(initial)
+        }
+
+        fn other_exit(&mut self, _msg: &NoMessages) {
+            self.other_exit_cnt += 1;
+        }
+    );
+
+    let mut sm = Test::new();
+    assert_eq!(sm.initial_base_enter_cnt, 0);
+    assert_eq!(sm.initial_base_cnt, 0);
+    assert_eq!(sm.initial_base_exit_cnt, 0);
+    assert_eq!(sm.initial_enter_cnt, 0);
+    assert_eq!(sm.initial_cnt, 0);
+    assert_eq!(sm.initial_exit_cnt, 0);
+    assert_eq!(sm.other_base_enter_cnt, 0);
+    assert_eq!(sm.other_base_cnt, 0);
+    assert_eq!(sm.other_base_exit_cnt, 0);
+    assert_eq!(sm.other_enter_cnt, 0);
+    assert_eq!(sm.other_cnt, 0);
+    assert_eq!(sm.other_exit_cnt, 0);
+
+    sm.dispatch(&NoMessages);
+    assert_eq!(sm.initial_base_enter_cnt, 1);
+    assert_eq!(sm.initial_base_cnt, 0);
+    assert_eq!(sm.initial_base_exit_cnt, 1);
+    assert_eq!(sm.initial_enter_cnt, 1);
+    assert_eq!(sm.initial_cnt, 1);
+    assert_eq!(sm.initial_exit_cnt, 1);
+    assert_eq!(sm.other_base_enter_cnt, 0);
+    assert_eq!(sm.other_base_cnt, 0);
+    assert_eq!(sm.other_base_exit_cnt, 0);
+    assert_eq!(sm.other_enter_cnt, 0);
+    assert_eq!(sm.other_cnt, 0);
+    assert_eq!(sm.other_exit_cnt, 0);
+
+    sm.dispatch(&NoMessages);
+    assert_eq!(sm.initial_base_enter_cnt, 1);
+    assert_eq!(sm.initial_base_cnt, 0);
+    assert_eq!(sm.initial_base_exit_cnt, 1);
+    assert_eq!(sm.initial_enter_cnt, 1);
+    assert_eq!(sm.initial_cnt, 1);
+    assert_eq!(sm.initial_exit_cnt, 1);
+    assert_eq!(sm.other_base_enter_cnt, 1);
+    assert_eq!(sm.other_base_cnt, 0);
+    assert_eq!(sm.other_base_exit_cnt, 1);
+    assert_eq!(sm.other_enter_cnt, 1);
+    assert_eq!(sm.other_cnt, 1);
+    assert_eq!(sm.other_exit_cnt, 1);
+
+    sm.dispatch(&NoMessages);
+    assert_eq!(sm.initial_base_enter_cnt, 2);
+    assert_eq!(sm.initial_base_cnt, 0);
+    assert_eq!(sm.initial_base_exit_cnt, 2);
+    assert_eq!(sm.initial_enter_cnt, 2);
+    assert_eq!(sm.initial_cnt, 2);
+    assert_eq!(sm.initial_exit_cnt, 2);
+    assert_eq!(sm.other_base_enter_cnt, 1);
+    assert_eq!(sm.other_base_cnt, 0);
+    assert_eq!(sm.other_base_exit_cnt, 1);
+    assert_eq!(sm.other_enter_cnt, 1);
+    assert_eq!(sm.other_cnt, 1);
+    assert_eq!(sm.other_exit_cnt, 1);
+
+    sm.dispatch(&NoMessages);
+    assert_eq!(sm.initial_base_enter_cnt, 2);
+    assert_eq!(sm.initial_base_cnt, 0);
+    assert_eq!(sm.initial_base_exit_cnt, 2);
+    assert_eq!(sm.initial_enter_cnt, 2);
+    assert_eq!(sm.initial_cnt, 2);
+    assert_eq!(sm.initial_exit_cnt, 2);
+    assert_eq!(sm.other_base_enter_cnt, 2);
+    assert_eq!(sm.other_base_cnt, 0);
+    assert_eq!(sm.other_base_exit_cnt, 2);
+    assert_eq!(sm.other_enter_cnt, 2);
+    assert_eq!(sm.other_cnt, 2);
+    assert_eq!(sm.other_exit_cnt, 2);
+
+    sm.dispatch(&NoMessages);
+    assert_eq!(sm.initial_base_enter_cnt, 3);
+    assert_eq!(sm.initial_base_cnt, 0);
+    assert_eq!(sm.initial_base_exit_cnt, 3);
+    assert_eq!(sm.initial_enter_cnt, 3);
+    assert_eq!(sm.initial_cnt, 3);
+    assert_eq!(sm.initial_exit_cnt, 3);
+    assert_eq!(sm.other_base_enter_cnt, 2);
+    assert_eq!(sm.other_base_cnt, 0);
+    assert_eq!(sm.other_base_exit_cnt, 2);
+    assert_eq!(sm.other_enter_cnt, 2);
+    assert_eq!(sm.other_cnt, 2);
+    assert_eq!(sm.other_exit_cnt, 2);
+}
