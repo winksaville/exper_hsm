6731b55bc5bc75d0679d48bdb4545d9d8d1e7bbf
Wink Saville <wink@saville.com>
2022-08-18 12:45:56 -0700

    feat: Use duct::cmd instead of process::Command
    This is "simpler" and this is what was used in the Rust Ferris Club
    rust_starter example: https://github.com/rusty-ferris-club/rust-starter.
    
    Add clippy to pre-commit and some testing that cargo is installed.

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,14 +1,15 @@
+use duct::cmd;
 use std::{
     env,
     path::{Path, PathBuf},
-    process::Command,
+    process::exit,
 };
 
 type DynError = Box<dyn std::error::Error>;
 
 fn main() -> Result<(), DynError> {
     let task = env::args().nth(1);
-    match task.as_ref().map(|it| it.as_str()) {
+    match task.as_deref() {
         Some("pre-commit") => pre_commit()?,
         _ => print_help(),
     }
@@ -18,48 +19,65 @@ fn main() -> Result<(), DynError> {
 fn print_help() {
     eprintln!(
         r#"Tasks:
-pre-commit:    Runs `cargo fmt` and `cargo test`"#
+pre-commit:    Runs `cargo fmt`, `cargo clippy` and `cargo test`"#
     );
 }
 
 fn pre_commit() -> Result<(), DynError> {
     cargo_fmt()?;
+    cargo_clippy()?;
     cargo_test()?;
+
+    Ok(())
+}
+
+fn cargo_clippy() -> Result<(), DynError> {
+    eprintln!("Run cargo clippy");
+    cmd!(cargo_string(), "clippy").dir(project_root()).run()?;
 
     Ok(())
 }
 
 fn cargo_fmt() -> Result<(), DynError> {
     eprintln!("Run cargo fmt");
-    let cargo = env::var("CARGO").unwrap_or_else(|_| "cargo".to_string());
-    let status = Command::new(cargo)
-        .current_dir(project_root())
-        .arg("fmt")
-        .status()?;
-
-    if !status.success() {
-        Err("cargo build failed")?;
-    }
+    cmd!(cargo_string(), "fmt").dir(project_root()).run()?;
 
     Ok(())
 }
 
 fn cargo_test() -> Result<(), DynError> {
     eprintln!("Run cargo test");
-    let cargo = env::var("CARGO").unwrap_or_else(|_| "cargo".to_string());
-    let status = Command::new(cargo)
-        .current_dir(project_root())
-        .arg("test")
-        .status()?;
-
-    if !status.success() {
-        Err("cargo test failed")?;
-    }
+    cmd!(cargo_string(), "test").dir(project_root()).run()?;
 
     Ok(())
 }
 
+fn cargo_string() -> String {
+    match env::var("CARGO") {
+        Ok(cs) => cs,
+        Err(_) => {
+            eprintln!("No CARGO environment variable, is cargo installed?");
+            exit(2);
+        }
+    }
+}
+
 fn project_root() -> PathBuf {
+    //println!("env!(CARGO): {:?}", env!("CARGO"));
+    //println!("env!(CARGO_MANIFEST_DIR): {:?}", env!("CARGO_MANIFEST_DIR"));
+    //let dot_dir = Path::new(".");
+    //println!("dot_dir={:?}", dot_dir);
+    //let bin_dir = Path::new(&env!("CARGO"));
+    //println!("bin_dir={:?}", bin_dir);
+    //let man_dir = Path::new(&env!("CARGO_MANIFEST_DIR"));
+    //println!("project_root(): man_dir={man_dir:?}");
+    //let mut ansestors = man_dir.ancestors();
+    //println!("project_root(): ansestors={:?}", &ansestors);
+    //let nth1 = ansestors.nth(1).unwrap();
+    //println!("project_root(): nth1={:?}", &nth1);
+    //let pb = nth1.to_path_buf();
+    //println!("project_root(): pb={pb:?}");
+    //pb
     Path::new(&env!("CARGO_MANIFEST_DIR"))
         .ancestors()
         .nth(1)
