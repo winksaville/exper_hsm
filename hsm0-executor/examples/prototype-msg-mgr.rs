dfce4fc176f67bfc56e9dba41cca49cd9520c003
Wink Saville <wink@saville.com>
2022-11-01 09:33:59 -0700

    feat: Add defer support to MsgMgr

--- hsm0-executor/examples/prototype-msg-mgr.rs
+++ hsm0-executor/examples/prototype-msg-mgr.rs
@@ -1,32 +1,64 @@
-use std::sync::mpsc::{Sender, Receiver};
+use std::sync::mpsc::{Sender, Receiver, RecvError, SendError, TryRecvError};
 
 use custom_logger::env_logger_init;
 
 use hsm0_executor::{DynError, Executor, StateInfo, StateResult, Handled};
 
 #[derive(Debug)]
+#[allow(unused)]
 pub struct MsgMgr<M> {
-    tx: Sender<M>,
-    rx: Receiver<M>,
+    primary_tx: Sender<M>,
+    primary_rx: Receiver<M>,
+    defer_tx: [Sender<M>; 2],
+    defer_rx: [Receiver<M>; 2],
+    current_defer_idx: usize,
 }
 
+#[allow(unused)]
 impl<M> MsgMgr<M> {
     fn new() -> MsgMgr<M> {
-        let (tx, rx) = std::sync::mpsc::channel::<M>();
+        let (primary_tx, primary_rx) = std::sync::mpsc::channel::<M>();
+        let (defer0_tx, defer0_rx) = std::sync::mpsc::channel::<M>();
+        let (defer1_tx, defer1_rx) = std::sync::mpsc::channel::<M>();
 
         MsgMgr {
-            tx,
-            rx,
+            primary_tx,
+            primary_rx,
+            defer_tx: [defer0_tx, defer1_tx],
+            defer_rx: [defer0_rx, defer1_rx],
+            current_defer_idx: 0,
         }
     }
 
-    fn recveiver(&self) -> &Receiver<M> {
-        &self.rx
+    fn recv(&self) -> Result<M, RecvError> {
+        self.primary_rx.recv()
+    }
+
+    fn send(&self, m: M) -> Result<(), SendError<M>>  {
+        self.primary_tx.send(m)
     }
 
     fn clone_sender(&self) -> Sender<M> {
-        self.tx.clone()
+        self.primary_tx.clone()
     }
+
+    fn defer_try_recv(&self) -> Result<M, TryRecvError> {
+        self.defer_rx[self.current_defer_idx].try_recv()
+    }
+
+    fn defer_send(&self,m: M) -> Result<(), SendError<M>> {
+        self.defer_tx[self.current_defer_idx].send(m)
+    }
+
+    fn current_defer(&self) -> (&Sender<M>, &Receiver<M>) {
+        (&self.defer_tx[self.current_defer_idx], &self.defer_rx[self.current_defer_idx])
+    }
+
+    fn other_defer(&self) -> (&Sender<M>, &Receiver<M>) {
+        (&self.defer_tx[(self.current_defer_idx + 1) % self.defer_tx.len()],
+        &self.defer_rx[(self.current_defer_idx + 1) % self.defer_rx.len()])
+    }
+
 }
 
 #[derive(Debug, Clone)]
@@ -40,18 +72,19 @@ enum Messages {
 }
 
 #[derive(Debug)]
-struct SendMsgToSelfSm {
-    self_tx: Sender<Messages>,
+struct SendMsgToSelfSm<'a> {
+    mm: &'a MsgMgr<Messages>,
     val: i32
 }
 
-const MAX_STATES: usize = 2;
+const MAX_STATES: usize = 3;
 const IDX_BASE: usize = 0;
-const IDX_DONE: usize = 0;
+const IDX_DONE: usize = 1;
+const IDX_PREPARE_TO_COMPLETE: usize = 2;
 
-impl SendMsgToSelfSm {
-    pub fn new(mm: &MsgMgr<Messages>) -> Result<Executor<Self, Messages>, DynError> {
-        let sm = SendMsgToSelfSm { self_tx: mm.clone_sender(), val: 0 };
+impl<'a> SendMsgToSelfSm<'a> {
+    pub fn new(mm: &'a MsgMgr<Messages>) -> Result<Executor<Self, Messages>, DynError> {
+        let sm = SendMsgToSelfSm { mm, val: 0 };
         let mut sme = Executor::new(sm, MAX_STATES);
 
         sme.state(StateInfo::new(
@@ -62,12 +95,17 @@ impl SendMsgToSelfSm {
             None,
         ))
         .state(StateInfo::new(
+            "prepare_to_done",
+            None,
+            Self::prepare_to_complete,
+            None,
+            None))
+        .state(StateInfo::new(
             "done",
             None,
             Self::done,
             None,
-            None,
-        ))
+            None))
         .initialize(IDX_BASE)
         .expect("Unexpected error initializing");
 
@@ -88,7 +126,7 @@ impl SendMsgToSelfSm {
                 if self.val < 10 {
                     // Doing work, 
                     self.val += val;
-                    if self.self_tx.send(msg.clone()).is_ok() {
+                    if self.mm.send(msg.clone()).is_ok() {
                         log::info!("base Messages::Value:- self.val={}", self.val);
                         (Handled::Yes, None)
                     } else {
@@ -101,7 +139,7 @@ impl SendMsgToSelfSm {
                     self.send_done();
 
                     log::info!("base Messages::Value:- Done self.val={}", self.val);
-                    (Handled::Yes, Some(IDX_DONE))
+                    (Handled::Yes, Some(IDX_PREPARE_TO_COMPLETE))
                 }
             }
             Messages::Done { val: _ } => {
@@ -111,15 +149,22 @@ impl SendMsgToSelfSm {
         }
     }
 
+    fn prepare_to_complete(&mut self, _msg: &Messages) -> StateResult {
+        // Responsed with Done for any messages
+        //self.send_done();
+        log::info!("prepare_to_complete:+- self.val={}", self.val);
+        (Handled::Yes, None)
+    }
+
     fn done(&mut self, _msg: &Messages) -> StateResult {
         // Responsed with Done for any messages
         self.send_done();
-        log::info!("base:+- self.val={}", self.val);
+        log::info!("done: self.val={}", self.val);
         (Handled::Yes, None)
     }
 
     fn send_done(&mut self) {
-        self.self_tx.send(Messages::Done { val: self.val }).ok();
+        self.mm.send(Messages::Done { val: self.val }).ok();
     }
 }
 
@@ -136,8 +181,7 @@ fn main() {
     sme.dispatch(&msg);
 
     // Receive messages until SendMsgToSelfSm reports Done or rx is closed
-    let rx = mm.recveiver();
-    while let Ok(m) = rx.recv() {
+    while let Ok(m) = mm.recv() {
         match m {
             Messages::Value { val: _ } => {
                 // Dispatch the message received
