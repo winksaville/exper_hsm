91fbfc47d933e146b800ff692e0bd766f5aa28c9
Wink Saville <wink@saville.com>
2022-11-19 12:33:10 -0800

    feat: Do not save Executor in StateMachine
    Since we are now passing Executor to ProcessFn it is unnecessary to
    save in StateMachine and thus unnecessary to make it a
    Rc<RefCell<Executor>>.

--- hsm0-executor/src/main.rs
+++ hsm0-executor/src/main.rs
@@ -4,9 +4,6 @@ use std::{cell::RefCell, rc::Rc};
 use hsm0_executor::{Executor, Handled, StateInfo, StateResult};
 
 pub struct StateMachine {
-    // Use MaybeUninit or some other techinique so get_sme()
-    // can use StateMachine.sme without checking.
-    sme: Option<Rc<RefCell<Executor<Self, NoMessages>>>>,
     state: i32,
 }
 
@@ -17,41 +14,22 @@ const MAX_STATES: usize = 1;
 const IDX_STATE1: usize = 0;
 
 impl StateMachine {
-    fn new() -> Rc<RefCell<Executor<Self, NoMessages>>> {
+    fn new() -> Executor<Self, NoMessages> {
         let sm = Rc::new(RefCell::new(StateMachine {
-            sme: None,
             state: 0,
         }));
 
-        let sme = Rc::new(RefCell::new(Executor::new(Rc::clone(&sm), MAX_STATES)));
-        sm.borrow_mut().sme = Some(Rc::clone(&sme));
+        let mut sme = Executor::new(Rc::clone(&sm), MAX_STATES);
 
-        sme.borrow_mut()
+        sme
             .state(StateInfo::new("state1", None, Self::state1, None, None))
             .initialize(IDX_STATE1)
             .expect("Unexpected error initializing");
 
-        let x = sm.borrow().get_sme();
-
-        // Validate initialization complete, panic if not
-        if sm.borrow_mut().sme.is_none() {
-            panic!("StateMacine.sme is not initialized");
-        }
-
         sme
     }
 
-    fn get_sme(&self) -> Rc<RefCell<Executor<Self, NoMessages>>> {
-        match &self.sme {
-            Some(sme) => Rc::clone(sme),
-            None => panic!("StateMachine.sme is not initialized"),
-        }
-    }
-
     fn state1(&mut self, e: &Executor<Self, NoMessages>, _msg: &NoMessages) -> StateResult {
-        // Enabling the first println! below causes:
-        //  thread 'main' panicked at 'already mutably borrowed: BorrowError', hsm0-executor/src/main.rs:54:41
-        //println!("{}:+", self.get_sme().borrow().get_state_name(IDX_STATE1));
         println!("{}:+", e.get_state_name(IDX_STATE1));
 
         self.state += 1;
@@ -66,24 +44,24 @@ fn main() {
     println!("main:+");
 
     // Create a sme and validate it's in the expected state
-    let sme = StateMachine::new();
-    assert_eq!(std::mem::size_of_val(sme.borrow().get_sm()), 8);
-    assert_eq!(sme.borrow().get_state_enter_cnt(IDX_STATE1), 0);
-    assert_eq!(sme.borrow().get_state_process_cnt(IDX_STATE1), 0);
-    assert_eq!(sme.borrow().get_state_exit_cnt(IDX_STATE1), 0);
-    assert_eq!(sme.borrow().get_sm().borrow().state, 0);
+    let mut sme = StateMachine::new();
+    assert_eq!(std::mem::size_of_val(sme.get_sm()), 8);
+    assert_eq!(sme.get_state_enter_cnt(IDX_STATE1), 0);
+    assert_eq!(sme.get_state_process_cnt(IDX_STATE1), 0);
+    assert_eq!(sme.get_state_exit_cnt(IDX_STATE1), 0);
+    assert_eq!(sme.get_sm().borrow().state, 0);
 
-    sme.borrow_mut().dispatch(&NoMessages);
-    assert_eq!(sme.borrow().get_state_enter_cnt(IDX_STATE1), 0);
-    assert_eq!(sme.borrow().get_state_process_cnt(IDX_STATE1), 1);
-    assert_eq!(sme.borrow().get_state_exit_cnt(IDX_STATE1), 0);
-    assert_eq!(sme.borrow().get_sm().borrow().state, 1);
+    sme.dispatch(&NoMessages);
+    assert_eq!(sme.get_state_enter_cnt(IDX_STATE1), 0);
+    assert_eq!(sme.get_state_process_cnt(IDX_STATE1), 1);
+    assert_eq!(sme.get_state_exit_cnt(IDX_STATE1), 0);
+    assert_eq!(sme.get_sm().borrow().state, 1);
 
-    sme.borrow_mut().dispatch(&NoMessages);
-    assert_eq!(sme.borrow().get_state_enter_cnt(IDX_STATE1), 0);
-    assert_eq!(sme.borrow().get_state_process_cnt(IDX_STATE1), 2);
-    assert_eq!(sme.borrow().get_state_exit_cnt(IDX_STATE1), 0);
-    assert_eq!(sme.borrow().get_sm().borrow().state, 2);
+    sme.dispatch(&NoMessages);
+    assert_eq!(sme.get_state_enter_cnt(IDX_STATE1), 0);
+    assert_eq!(sme.get_state_process_cnt(IDX_STATE1), 2);
+    assert_eq!(sme.get_state_exit_cnt(IDX_STATE1), 0);
+    assert_eq!(sme.get_sm().borrow().state, 2);
 
     println!("main:-");
 }
