d3a45a053cb373d4f522c0c2771f410bb4bbf825
Wink Saville <wink@saville.com>
2022-11-19 12:40:38 -0800

    feat: Add state2
    And transition between state1 and state2

--- hsm0-executor/src/main.rs
+++ hsm0-executor/src/main.rs
@@ -10,19 +10,18 @@ pub struct StateMachine {
 // Create a Protocol
 pub struct NoMessages;
 
-const MAX_STATES: usize = 1;
+const MAX_STATES: usize = 2;
 const IDX_STATE1: usize = 0;
+const IDX_STATE2: usize = 1;
 
 impl StateMachine {
     fn new() -> Executor<Self, NoMessages> {
-        let sm = Rc::new(RefCell::new(StateMachine {
-            state: 0,
-        }));
+        let sm = Rc::new(RefCell::new(StateMachine { state: 0 }));
 
         let mut sme = Executor::new(Rc::clone(&sm), MAX_STATES);
 
-        sme
-            .state(StateInfo::new("state1", None, Self::state1, None, None))
+        sme.state(StateInfo::new("state1", None, Self::state1, None, None))
+            .state(StateInfo::new("state2", None, Self::state2, None, None))
             .initialize(IDX_STATE1)
             .expect("Unexpected error initializing");
 
@@ -35,7 +34,16 @@ impl StateMachine {
         self.state += 1;
 
         println!("{}:-", e.get_state_name(IDX_STATE1));
-        (Handled::Yes, None)
+        (Handled::Yes, Some(IDX_STATE2))
+    }
+
+    fn state2(&mut self, e: &Executor<Self, NoMessages>, _msg: &NoMessages) -> StateResult {
+        println!("{}:+", e.get_state_name(IDX_STATE2));
+
+        self.state -= 1;
+
+        println!("{}:-", e.get_state_name(IDX_STATE2));
+        (Handled::Yes, Some(IDX_STATE1))
     }
 }
 
@@ -49,19 +57,28 @@ fn main() {
     assert_eq!(sme.get_state_enter_cnt(IDX_STATE1), 0);
     assert_eq!(sme.get_state_process_cnt(IDX_STATE1), 0);
     assert_eq!(sme.get_state_exit_cnt(IDX_STATE1), 0);
+    assert_eq!(sme.get_state_enter_cnt(IDX_STATE2), 0);
+    assert_eq!(sme.get_state_process_cnt(IDX_STATE2), 0);
+    assert_eq!(sme.get_state_exit_cnt(IDX_STATE2), 0);
     assert_eq!(sme.get_sm().borrow().state, 0);
 
     sme.dispatch(&NoMessages);
     assert_eq!(sme.get_state_enter_cnt(IDX_STATE1), 0);
     assert_eq!(sme.get_state_process_cnt(IDX_STATE1), 1);
     assert_eq!(sme.get_state_exit_cnt(IDX_STATE1), 0);
+    assert_eq!(sme.get_state_enter_cnt(IDX_STATE2), 0);
+    assert_eq!(sme.get_state_process_cnt(IDX_STATE2), 0);
+    assert_eq!(sme.get_state_exit_cnt(IDX_STATE2), 0);
     assert_eq!(sme.get_sm().borrow().state, 1);
 
     sme.dispatch(&NoMessages);
     assert_eq!(sme.get_state_enter_cnt(IDX_STATE1), 0);
-    assert_eq!(sme.get_state_process_cnt(IDX_STATE1), 2);
+    assert_eq!(sme.get_state_process_cnt(IDX_STATE1), 1);
     assert_eq!(sme.get_state_exit_cnt(IDX_STATE1), 0);
-    assert_eq!(sme.get_sm().borrow().state, 2);
+    assert_eq!(sme.get_state_enter_cnt(IDX_STATE2), 0);
+    assert_eq!(sme.get_state_process_cnt(IDX_STATE2), 1);
+    assert_eq!(sme.get_state_exit_cnt(IDX_STATE2), 0);
+    assert_eq!(sme.get_sm().borrow().state, 0);
 
     println!("main:-");
 }
